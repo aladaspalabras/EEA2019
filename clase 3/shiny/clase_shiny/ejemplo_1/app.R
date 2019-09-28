74203b774a4762bcbe0bef11179edcc0bdd6ba8a
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2019-09-28 13:04:12 -0300

    Ejemplo de shiny comentado

--- clase 3/shiny/clase_shiny/ejemplo_1/app.R
+++ clase 3/shiny/clase_shiny/ejemplo_1/app.R
@@ -15,9 +15,9 @@ ui <- fluidPage(
     titlePanel("Old Faithful Geyser Data"),
 
     # Sidebar con slider input para el numero de bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
+    sidebarLayout(                     # nos dice que hay un elemento en el centro y otro al costado
+        sidebarPanel(                          # lo que queremos que vaya al costadito
+            sliderInput("bins",                # metemos un widget
                         "Number of bins:",
                         min = 1,
                         max = 50,
@@ -25,8 +25,8 @@ ui <- fluidPage(
         ),
 
         # Mostramos un grafico con la distribucion
-        mainPanel(
-           plotOutput("distPlot")
+        mainPanel(                            
+           plotOutput("distPlot")             # indica que queremos mostrar un gráfico (plot)
         )
     )
 )
@@ -34,14 +34,14 @@ ui <- fluidPage(
 # Definimos la funcion server
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$distPlot <- renderPlot({          # generamos un output con código dinámico e interactivo
         #  Generamos los bins basados en input$bins del UI
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # Generamos el histograma
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    }) # cada vez que el usuario modifica un input, R vuelve a correr esta porción de código
 }
 
 # Ejecutamos la aplicacion. 
